7a6bb9f1a5aa3a23e8120c69c4288753739f03ce
inessaid <43462511+inessaid@users.noreply.github.com>
2020-11-07 17:57:14 -0500

    some items added

---
+++ Assets/GetStartedManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetStartedManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public  GameObject signuppanel,getstartedpanel;
+    public void ButtonClick()
+    {
+        getstartedpanel.SetActive(false);
+        signuppanel.SetActive(true);
+    }
+}
